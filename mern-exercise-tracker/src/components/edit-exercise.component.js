400b357a1aeef1a01b2185b667166de1420db277
Shervantha Nirmal Hugh De Silva <shevids@gmail.com>
2019-07-21 15:43:28 +1000

    Added components for frontend

---
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -0,0 +1,12 @@
+import React,{ Component  } from "react";
+
+export default class EditExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are in the Edit s Exercises component</p>
+            </div>
+        )
+    }
+}
+
